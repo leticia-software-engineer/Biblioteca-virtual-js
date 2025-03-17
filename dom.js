727ddc65b777ad0148615060c3ca7dd93daa00e9
Leticia Dias <devv.leticia@gmail.com>
2025-03-17 20:29:24 -0300

    Update dom.js

--- dom.js
+++ dom.js
@@ -1,39 +1,45 @@
+var caixa1 = document.getElementById('num')
+var caixa2 = document.getElementById('num2')
+var result = document.getElementById('result')
+
+
 function somar(){
-    var caixa1 = document.getElementById('num')
-    var caixa2 = document.getElementById('num2')
-    var result = document.getElementById('result')
     var numero1 = Number(caixa1.value)
     var numero2 = Number(caixa2.value)
     var som = numero1 + numero2
-    result.innerHTML = som
+    result.innerHTML = "O resultado da soma entre " +
+    numero1 + "+" + numero2 + " foi:  " + som
+    
 }
 
 function sub(){
-    var caixa1 = document.getElementById('num')
-    var caixa2 = document.getElementById('num2')
-    var result = document.getElementById('result')
     var numero1 = Number(caixa1.value)
     var numero2 = Number(caixa2.value)
     var subt = numero1 - numero2
-    result.innerHTML = subt
+    result.innerHTML = "O resultado da subtração entre " +
+    numero1 + "-" + numero2 + " foi:  " + subt 
 }
 
 function mult(){
-    var caixa1 = document.getElementById("num")
-    var caixa2 = document.getElementById("num2")
-    var result = document.getElementById('result')
     var numero1 = Number(caixa1.value)
     var numero2 = Number(caixa2.value)
     var multp = numero1 * numero2
-    result.innerHTML = multp
+    result.innerHTML =  "O resultado da multiplicacão entre " +
+    numero1 + " x " + numero2 + " foi:  " + multp
 }
 
 function div(){
-    var caixa1 = document.getElementById("num")
-    var caixa2 = document.getElementById("num2")
-    var result = document.getElementById("result")
     var numero1 = Number(caixa1.value)
     var numero2 = Number(caixa2.value)
     var divis = numero1 / numero2
-    result.innerHTML = divis
+    result.innerHTML = "O resultado da divisão entre " +
+    numero1 + " / " + numero2 + " foi:  " + divis
+}
+
+function pot(){
+    var numero1 = Number(caixa1.value)
+    var numero2 = Number(caixa2.value)
+    var poten = numero1 ** numero2
+    result.innerHTML = "O resultado da potência de base " +
+    numero1 + " e expoente " + numero2 + " foi:  " + poten
 }
